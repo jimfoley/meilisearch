91fa39c213035fc6b1289df55144995df20b3093
Jim Foley <jfoley@bestphotoprint.com>
2022-10-21 17:36:26 -0600

    add react components for ui

--- app/javascript/recipes/App.jsx
+++ app/javascript/recipes/App.jsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { InstantSearch, Highlight, SearchBox, Hits } from 'react-instantsearch-dom';
+import { InstantSearch, Highlight, SearchBox, Hits, RefinementList, ClearRefinements } from 'react-instantsearch-dom';
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 
 const searchClient = instantMeiliSearch(
-  "http://127.0.0.1:7700", // Your Meilisearch host
-  "" // Your Meilisearch API key, if you have set one
+  "http://127.0.0.1:7700",
+  ""
 );
 
 const App = () => (
@@ -12,8 +12,16 @@ const App = () => (
     indexName="Recipe" // Change your index name here
     searchClient={searchClient}
   >
-    <SearchBox />
-    <Hits hitComponent={Hit} />
+    <div className="left-panel">
+      <ClearRefinements />
+      <h2>Type of diet</h2>
+      <RefinementList attribute="diet" />
+    </div>
+    <div className="right-panel">
+      <SearchBox />
+      <Hits hitComponent={Hit} />
+    </div>
+
   </InstantSearch>
 );
 
