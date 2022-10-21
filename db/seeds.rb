de74ad19ec4bfdd46e7337b0ad1234a1be21f229
Jim Foley <jfoley@bestphotoprint.com>
2022-10-21 14:49:56 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Deletes existing recipes, useful if you seed several times
+Recipe.destroy_all
+
+# Creates 1000 fake recipes
+1000.times do
+    Recipe.create!(
+        title: "#{Faker::Food.dish} by #{Faker::Name.unique.name}",
+        ingredients: "#{Faker::Food.ingredient}, #{Faker::Food.ingredient}, #{Faker::Food.ingredient}",
+        directions: Faker::Food.description,
+        diet: ['omnivore', 'pescetarian', 'vegetarian', 'vegan'].sample
+    )
+end 
+
+# Displays the following message in the console once the seeding is done
+puts 'Recipes created'
